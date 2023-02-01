28f5cf4f0698e3338701d2015401a8ed8819e4c5
Alex Sinfarosa <alexsinfarosa@users.noreply.github.com>
2023-02-01 17:39:42 +0100

    updates

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,20 @@
 import {Link} from '@remix-run/react'
 
+const linkStyle = {
+  padding: '1rem',
+  border: '1px solid #eee',
+}
+
 export default function IndexPage() {
   return (
     <div className="container">
       <h1>Index page</h1>
       <p>Available Routes:</p>
-      <Link to="posts">posts</Link>
+      <div className="grid">
+        <Link to="posts" style={{...linkStyle}}>
+          /posts
+        </Link>
+      </div>
     </div>
   )
 }
